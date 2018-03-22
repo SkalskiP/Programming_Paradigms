90a4741fb711f3830b6b0597e5b9b0367a60ecb9
SkalskiP <pskalski@student.agh.edu.pl>
2018-03-22 12:19:42 +0100

    lab 4

---
+++ Lab_3/listy.pl
@@ -0,0 +1,18 @@
+porownaj([_,_,P,P|_]).
+
+nalezy(X,[X|_]).
+nalezy(X,[_|Yogon]) :-
+	nalezy(X,Yogon).
+
+dlugosc([],0).
+dlugosc([_|Ogon],Dlug) :-
+	dlugosc(Ogon,X),
+	Dlug is X+1.
+
+a2b([],[]).
+a2b([a|Ta],[b|Tb]) :- 
+   a2b(Ta,Tb).
+
+sklej([],X,X).
+sklej([X|L1],L2,[X|L3]) :-
+	sklej(L1,L2,L3).
